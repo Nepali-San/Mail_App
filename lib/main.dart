7d9ae69f257ee464d22845bc5a178c54dc95900f
San Neupane <123here.comes.san@gmail.com>
2019-08-19 20:51:21 +0545

    dislay msg list

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,51 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(EmailApp());
 
-class MyApp extends StatelessWidget {  
+class EmailApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(       
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
         primarySwatch: Colors.blue,
+        accentColor: Colors.lightBlueAccent,
       ),
-      home: Container(),
+      home: HomePage(),
     );
   }
 }
 
+class HomePage extends StatelessWidget {
+  final messages = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Email App'),
+      ),
+      body: ListView.separated(
+        separatorBuilder: (context, index) => Divider(),
+        itemBuilder: (BuildContext context, int index) {
+          final Map<String, String> message = messages[index];
+
+          return ListTile(
+            title: Text(message['subject']),
+            subtitle: Text(
+              message['body'],
+              maxLines: 2,
+              overflow: TextOverflow.ellipsis,
+            ),
+            leading: CircleAvatar(
+              child: Text("A"),
+            ),
+            isThreeLine: true,
+          );
+        },
+        itemCount: messages.length,
+      ),
+    );
+  }
+}
