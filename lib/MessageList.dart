5a51d5a8dc031439a3e2557498c843c08e0eade1
San Neupane <123here.comes.san@gmail.com>
2019-08-21 20:14:55 +0545

    http request to get messages

--- lib/MessageList.dart
+++ lib/MessageList.dart
@@ -1,7 +1,4 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:mail_app_practise/model/message.dart';
 
 class MessageList extends StatefulWidget {
@@ -11,28 +8,13 @@ class MessageList extends StatefulWidget {
 
 class _MessageListState extends State<MessageList> {
   List<Message> messages = [];
+  bool isLoading = true;
 
   Future loadMessageList() async {
-    //load the json file.
-    String content = await rootBundle.loadString('data/message.json');
-
-    //decode the json, since first symbol is square bracket , our json is the "list of dynamic" so it return list<dynamic>
-    //if first symbol was { then it would return Map<String,dynamic>.
-
-    List<dynamic> collection = json.decode(content);
-
+    List<Message> _messages = await Message.browse();
     setState(() {
-      //use contructor to typecast the json into object.
-      messages = collection.map((json) => Message.fromJson(json)).toList();
-
-      // above contructor does the same task as below constructor.
-      //   messages = collection
-      //       .map((json) => Message(
-      //             subject: json['subject'],
-      //             body: json['body'],
-      //           ))
-      //       .toList();
-      // });
+      messages = _messages;
+      isLoading = false;
     });
   }
 
@@ -48,26 +30,28 @@ class _MessageListState extends State<MessageList> {
       appBar: AppBar(
         title: Text('Email App'),
       ),
-      body: ListView.separated(
-        separatorBuilder: (context, index) => Divider(),
-        itemBuilder: (BuildContext context, int index) {
-          Message message = messages[index];
+      body: isLoading
+          ? Center(child: CircularProgressIndicator())
+          : ListView.separated(
+              separatorBuilder: (context, index) => Divider(),
+              itemBuilder: (BuildContext context, int index) {
+                Message message = messages[index];
 
-          return ListTile(
-            title: Text(message.subject),
-            subtitle: Text(
-              message.body,
-              maxLines: 2,
-              overflow: TextOverflow.ellipsis,
+                return ListTile(
+                  title: Text(message.subject),
+                  subtitle: Text(
+                    message.body,
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  leading: CircleAvatar(
+                    child: Text("A"),
+                  ),
+                  isThreeLine: true,
+                );
+              },
+              itemCount: messages.length,
             ),
-            leading: CircleAvatar(
-              child: Text("A"),
-            ),
-            isThreeLine: true,
-          );
-        },
-        itemCount: messages.length,
-      ),
     );
   }
 }
--- lib/model/message.dart
+++ lib/model/message.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:json_annotation/json_annotation.dart';
+import 'package:http/http.dart' as http;
 
 part 'message.g.dart';
 
@@ -13,4 +16,33 @@ class Message {
       _$MessageFromJson(json);
 
   Map<String, dynamic> toJson() => _$MessageToJson(this);
+
+  static Future<List<Message>> browse() async {
+    //load the json file.
+    // String content = await rootBundle.loadString('data/message.json');
+
+    http.Response response = await http.get(
+        "http://www.mocky.io/v2/5d5d4f4d330000078057b924??mocky-delay=2000ms");
+    String content = response.body;
+
+    //decode the json, since first symbol is square bracket , our json is the "list of dynamic" so it return list<dynamic>
+    //if first symbol was { then it would return Map<String,dynamic>.
+
+    List<dynamic> collection = json.decode(content);
+
+    //use named contructor to typecast the json into object.
+    List<Message> _messages =
+        collection.map((json) => Message.fromJson(json)).toList();
+
+    // above contructor does the same task as below constructor.
+    //   messages = collection
+    //       .map((json) => Message(
+    //             subject: json['subject'],
+    //             body: json['body'],
+    //           ))
+    //       .toList();
+    // });
+
+    return _messages;
+  }
 }
