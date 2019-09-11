7d932da6b60a6249c7998159c9c13ef064b7a127
San Neupane <123here.comes.san@gmail.com>
2019-09-11 11:21:56 +0545

    minor changes

--- lib/streams/InboxManager.dart
+++ lib/streams/InboxManager.dart
@@ -14,12 +14,7 @@ class InboxManager {
   final PublishSubject<String> _filterStatus = PublishSubject<String>();
   void setStatus(value) {
     _filterStatus.sink.add(value);
-  }
-
-  /*
-   ? Is there any way to get string (through stream) and output a List (through sink) using a single subject ?
-   ? May be a streamTransformer will work
-   */
+  }  
 
   final PublishSubject<List<Message>> _messageList =
       PublishSubject<List<Message>>();
@@ -31,3 +26,16 @@ class InboxManager {
     _messageList.close();
   }
 }
+
+/*
+   ? Is there any way to get string (through stream) and output a List (through sink) using a single subject ?
+   ? May be a streamTransformer will work
+
+   * no, having a single controller/subject will create a loop : 
+   * we will have to get input and listen the input to give new appropriate ouput
+   * on giving the new output we again listen to it as new input to give new appropriate output and it continues.
+   * 
+   * Also single controller can't be of different type , we will need two controller
+   * one for the input(event) and other for the output(state)
+   * then we listen to eventcontroller and map it to the statecontroller as like above.
+   */
